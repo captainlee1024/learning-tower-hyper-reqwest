842db32a21797686748a73d4d872c8f2ac5db571
Terry <644052732@qq.com>
2025-04-09 04:02:20 +0800

    chore: update main document

--- src/main.rs
+++ src/main.rs
@@ -53,24 +53,107 @@ use tracing_subscriber::{Layer, Registry};
 /// docker start jaeger
 /// ```
 ///
-/// 2、launch the echo server:
+/// 2、launch the prometheus using docker:
+///
+/// ```bash
+/// chmod -R 777 prometheus-data
+/// ```
+///
+/// delete the prometheus data:
+///
+/// ```bash
+/// sudo rm -rf prometheus-data/*
+/// ```
+///
+/// launch prometheus:
+///
+/// ```bash
+/// docker run -d \
+///   --name prometheus \
+///   --network host \
+///   -v $(pwd)/prometheus.yml:/etc/prometheus/prometheus.yml \
+///   -v $(pwd)/prometheus-data:/prometheus \
+///   prom/prometheus:latest \
+///   --web.listen-address=":19090" \
+///   --config.file=/etc/prometheus/prometheus.yml \
+///   --enable-feature=otlp-write-receiver
+/// ```
+///
+/// 3、 launch the grafana using docker:
+///
+/// ```bash
+/// chmod -R 777 grafana-data
+/// ```
+///
+/// delete the grafana data:
+///
+/// ```bash
+/// sudo rm -rf grafana-data/*
+/// ```
+///
+/// launch grafana:
+///
+/// ```bash
+/// docker run -d \
+///   --name grafana \
+///   --network host \
+///   -e "GF_SERVER_HTTP_PORT=13000" \
+///   -v $(pwd)/grafana-data:/var/lib/grafana \
+///   grafana/grafana:latest
+/// ```
+///
+/// 3、launch the echo server:
 ///
 /// ```bash
 /// cargo run
 /// ```
 ///
-/// 3、test with curl:
+/// 4、test with curl:
 ///
 /// ```bash
 /// curl -v -X POST -H "Authorization: Bearer token" -d "hello world" http://127.0.0.1:3000
 /// ```
 ///
-/// 4、check the trace in Jaeger UI:
+/// 5、check the trace in Jaeger UI:
 ///
 /// [open Jaeger UI in browser](http://localhost:16686/)
 ///
 /// select the Service name `hyper-tower-service` and select the Operation name `request`, click `Find Traces` to see the
 /// traces.
+///
+/// 6、 check the metrics in prometheus:
+///
+/// [open prometheus UI in browser](http://localhost:19090/)
+///
+/// open metrics explorer, select `http_request_total`, `http_request_duration_seconds_bucket` ... to see the metrics.
+///
+/// 7、 check the metrics in grafana:
+///
+/// [open grafana UI in browser](http://localhost:13000/)
+///
+/// add the prometheus data source: http://localhost:19090
+///
+/// create a new dashboard, add a new panel, select the prometheus data source, and select the metrics you want to see.
+///
+/// Grafana Chart Examples
+///
+/// 1. **Request Rate Line Chart (Time Series)**
+///     - **Purpose**: Show request rate per second over time.
+///     - **Query**: `rate(http_requests_total[5m])`
+///         - `rate`: Calculates per-second increase over a 5-minute window.
+///     - **Visualization**:
+///         - Type: Time Series
+///         - Config: X-axis: time, Y-axis: req/s, split by `method`
+///     - **Value**: Monitor request trends and detect peaks.
+///
+/// 2. **Duration Distribution Histogram (Histogram)**
+///     - **Purpose**: Display request duration distribution (your bar chart need).
+///     - **Query**: `http_request_duration_seconds_bucket{method="POST"}`
+///         - Uses Histogram bucket data directly.
+///     - **Visualization**:
+///         - Type: Histogram
+///         - Config: X-axis: `le` (bucket boundaries), Y-axis: count, unit: milliseconds
+///     - **Value**: Understand duration spread, e.g., most requests in low latency.
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // // 初始化 Tracing
